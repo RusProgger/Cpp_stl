a2f916baa5eca976881d71a1ac3f34bde87ba921
RusProgger <ruslantimka92@gmail.com>
2026-09-20 16:32:46 +0300

    Add vector method

---
+++ vector/vector.cpp
@@ -0,0 +1,19 @@
+#include <print>
+#include <vector>
+
+/*
+
+push_back(10); // добавить в конец
+pop_back();    // удалить последний
+empty();       // пустой?
+size();        // количество элементов
+numbers[0];    // доступ по индексу
+
+*/
+
+int main() {
+
+
+
+    return 0;
+}
