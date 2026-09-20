b4d7b5e5d1d1513b62d2b40c12a8c3323061537c
RusProgger <ruslantimka92@gmail.com>
2026-09-20 16:48:13 +0300

    Add stack info

--- stack/stack_info.cpp
+++ stack/stack_info.cpp
@@ -3,8 +3,20 @@
 
 int main() {    
 
+    int number;
+    std::stack<int> user_info;
+
+    print("Enter number stack: ");
+    std::cin >> number;
+
+    for(int i = 0; i < number; i++) {
+        int user_value;
+        std::cin >> user_value;
+        user_info.push(user_value);
+    }
 
     
+
 
     std::cin.get();
     return 0;
